b8228f00b206cb43f1a81b9645d28615befb0c09
cereceda1991 <cereceda1991@gmail.com>
2023-05-03 18:38:20 -0500

    firt commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+
+function App() {
+
+
+  return (<>
+    <div className='container_quizizz'>
+      <iframe src="https://quizizz.com/embed/quiz/6452e6d441fe6d001d50cbde" width="100%" height="640px" frameborder="0" allowfullscreen></iframe>
+    </div>
+  </>
+  );
+}
+
+export default App
